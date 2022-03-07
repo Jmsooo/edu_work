23941f7a8db2581dffd8f83e182eb70afc2d1d20
jiaming <jiaming_06@163.com>
2022-03-07 21:07:16 +0800

    查询课程列表信息

--- lagou_edu_home/src/main/java/com/lagou/service/CourseService.java
+++ lagou_edu_home/src/main/java/com/lagou/service/CourseService.java
@@ -1,7 +1,15 @@
 package com.lagou.service;
 
+import com.lagou.pojo.Course;
+
+import java.util.List;
+
 /**
  * 课程管理模块 Service 层接口
  */
 public interface CourseService {
+
+    public List<Course> findCourseList();
+
+
 }
--- lagou_edu_home/src/main/java/com/lagou/service/impl/CourseServiceImpl.java
+++ lagou_edu_home/src/main/java/com/lagou/service/impl/CourseServiceImpl.java
@@ -1,9 +1,23 @@
 package com.lagou.service.impl;
 
+import com.lagou.dao.CourseDao;
+import com.lagou.dao.impl.CourseDaoImpl;
+import com.lagou.pojo.Course;
 import com.lagou.service.CourseService;
+
+import java.util.List;
 
 /**
  * 课程管理模块 Service 层的实现类
  */
 public class CourseServiceImpl implements CourseService {
+
+    //创建 CourseDao
+    CourseDao courseDao = new CourseDaoImpl();
+
+    @Override
+    public List<Course> findCourseList() {
+        List<Course> courseList = courseDao.findCourseList();
+        return courseList;
+    }
 }
