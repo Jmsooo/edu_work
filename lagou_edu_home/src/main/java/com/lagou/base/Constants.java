93992d50a481b6429194e6bf8cbdeba8139c1f1f
jiaming <jiaming_06@163.com>
2022-03-10 14:24:09 +0800

    本地访问地址

--- lagou_edu_home/src/main/java/com/lagou/base/Constants.java
+++ lagou_edu_home/src/main/java/com/lagou/base/Constants.java
@@ -3,6 +3,6 @@ package com.lagou.base;
 public class Constants {
 
     //本地访问地址
-    static final String LOCAL_URL = "http://localhost:8080";
+    public static final String LOCAL_URL = "http://localhost:8080";
 
 }
